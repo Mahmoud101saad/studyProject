607720a80ccfc1cb624bbd1b4fb7ac5373e2677e
Mahmoud101saad <73835523+Mahmoud101saad@users.noreply.github.com>
2023-02-25 14:09:03 +0200

    asd

--- studyProject/Program.cs
+++ studyProject/Program.cs
@@ -6,7 +6,7 @@ namespace studyProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! s");
+            Console.WriteLine("Hello World!");
         }
     }
 }
